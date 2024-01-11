95566f71f9202b48e6da1037282beca888531c72
jitheshm <jitheshmjithooz@gmail.com>
2024-01-11 21:13:13 +0530

    search option added in admin dashboard

--- frontend/src/components/Admin/Dashboard/DashboardComponent.jsx
+++ frontend/src/components/Admin/Dashboard/DashboardComponent.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-key */
 import React, { useEffect, useState } from 'react'
 import './Dashboard.css'
 import { BASEURL } from '../../../constants/constant.json'
@@ -16,12 +17,17 @@ function DashboardComponent() {
             setUsers(res.data.data)
         })
     }, [])
+    const [search, setSearch] = useState("")
     return (
         <>
 
             <section className=" container-fluid">
                 <div className="col-lg-10 m-auto">
-                    <div className="d-flex justify-content-end ">
+                    <div className="d-flex justify-content-between align-items-center ">
+                        <input type="text" placeholder='search' style={{ height: '50px' }} value={search}
+                            onChange={(e) => {
+                                setSearch(e.target.value)
+                            }} />
                         <Link className="btn btn-success m-5" to={'/admin/create'}>Create</Link>
                     </div>
                     <div className="table-responsive">
@@ -37,21 +43,28 @@ function DashboardComponent() {
                             </thead>
                             <tbody>
                                 {
-                                    users.map((user,index) => {
-                                        return (
-                                            // eslint-disable-next-line react/jsx-key
-                                            <tr>
-                                                <th scope="row">{index+1}</th>
-                                                <td className="w-25">{user.name}</td> 
-                                                <td className="w-25">{user.email}</td>
-                                                <td className="d-flex">
-                                                    <div>
-                                                        <Link to={`/admin/edit/${user._id}`} className="btn btn-primary mx-2" >Edit</Link>
-                                                        <a className="btn btn-danger mx-2"  >Delete</a>
-                                                    </div>
-                                                </td>
-                                            </tr>
-                                        )
+                                    users.map((user, index) => {
+                                        if (user.name.startsWith(search) || search===""||user.email.includes(search)) {
+                                            return (
+                                                // eslint-disable-next-line react/jsx-key
+
+                                                <tr>
+                                                    <th scope="row">{index + 1}</th>
+                                                    <td className="w-25">{user.name}</td>
+                                                    <td className="w-25">{user.email}</td>
+                                                    <td className="d-flex">
+                                                        <div>
+                                                            <Link to={`/admin/edit/${user._id}`} className="btn btn-primary mx-2" >Edit</Link>
+                                                            <a className="btn btn-danger mx-2"  >Delete</a>
+                                                        </div>
+                                                    </td>
+                                                </tr>
+
+                                            )
+                                        }else{
+                                            return null
+                                        }
+
                                     })
                                 }
                             </tbody>
